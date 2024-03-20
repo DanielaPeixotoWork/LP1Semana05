dc3e86d35be8c82c512f6d69f7dd8cb8b53f719f
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-20 12:16:49 +0000

    Pedir ao utilizadroe números e semente

---
+++ RanDice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Uso: dotnet run --project RanDice -- <número de dados> <semente>");
+            return;
+        }
+
+        if (!int.TryParse(args[0], out int n) || !int.TryParse(args[1], out int s))
+        {
+            Console.WriteLine("Os argumentos fornecidos não são válidos.");
+            return;
+        }
+
+        }
+    }
+}
