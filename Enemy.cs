13b7de8d7b6745e7cc503616cf64508b82193a0b
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-20 12:49:50 +0000

    Exercise 7 finished

--- Enemy.cs
+++ Enemy.cs
@@ -1,7 +1,15 @@
 namespace MyGame
 {
     public class Enemy
+
+    private string name;
+    private float health;
+    private float shield;
+    
+    public class Enemy (string name)
     {
-        
+        this.name =name;
+        health = 100;
+        shield = 0;
     }
 }
