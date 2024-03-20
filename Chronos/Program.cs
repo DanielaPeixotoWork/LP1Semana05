7b6bfaf9673d40f2be06f9b273e38c2179ad9be8
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-03-20 11:46:16 +0000

    pausa o programa por 0.2 segundos

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -10,10 +10,23 @@ namespace Chronos
         {
 
         Stopwatch crono1 = new Stopwatch();
+        Stopwatch crono2 = new Stopwatch();
 
         crono1.Start();
 
         Thread.Sleep(600);
+
+        crono2.Start();
+
+        Thread.Sleep(200);
+
+        crono1.Stop();
+        crono2.Stop();
+
+
+        Console.WriteLine("Tempo decorrido em crono1: {0:F3} segundos", crono1.ElapsedMilliseconds / 1000.0);
+        Console.WriteLine("Tempo decorrido em crono2: {0:F3} segundos", crono2.ElapsedMilliseconds / 1000.0);
         }
     }
 }
+
